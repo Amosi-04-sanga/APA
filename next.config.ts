30a97f4bf1d5f6c52596d6a906884c2b27154c65
aptech889 <aptech889@gmail.com>
2025-07-25 20:43:41 +0300

    tsx to jsx again

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-   images: {
-    domains: ['randomuser.me'],
-  }
+  images: {
+    domains: ["randomuser.me"],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
